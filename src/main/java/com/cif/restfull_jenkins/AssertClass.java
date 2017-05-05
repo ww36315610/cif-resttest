66025c2c6f436c6b7310cd0bf3b020ebe250d40b
wangjian02@finupgroup.com <wangjian02@finupgroup.com>
2017-05-05 15:21:36 +0800

    add case  and  fileter  class

--- src/main/java/com/cif/restfull_jenkins/AssertClass.java
+++ src/main/java/com/cif/restfull_jenkins/AssertClass.java
@@ -13,13 +13,25 @@ public class AssertClass {
 	public void method(JSONObject object) {
 		try {
 			HttpResponse htr = (HttpResponse) ReturnClass.getHttpClass(object);
-			JSONArray array = htr.getResponse(object, "xy691fc4b8c804544369e6146d4314282e20160926");
+			// ALL:     xy2323bcac34a11a81934507afe12e9a2820160926
+			//QZ:       xy691fc4b8c804544369e6146d4314282e20160926
+			JSONArray array = htr.getResponse(object, " xy2323bcac34a11a81934507afe12e9a2820160926");
 			JSONObject json = null;
 			for (int i = 0; i < array.size(); i++) {
 				json = JSONObject.parseObject(array.get(i).toString());
 				Assert.assertEquals(json.get(object.get("code")), object.get("expecteds"), "返回码不正确！！！！！");
-				Assert.assertTrue(json.getJSONArray(object.get("code_1").toString()).size() >= Integer.parseInt(object
-						.get("expecteds_1").toString()));
+				if (object.get("code_1").toString().equals("tag")) {
+					Assert.assertTrue(json.getJSONArray(object.get("code_1").toString()).size() >= Integer
+							.parseInt(object.get("expecteds_1").toString()));
+				} else {
+					String expecteds = object.get("expecteds_1").toString();
+					int expNum = Integer.parseInt(expecteds);
+					String actual = json.getString(object.getString("code_1"));
+					int actNum = Integer.parseInt(actual);
+					System.out.println("[exp:" + expNum + "]---[act:" + actNum + "]---[param:"
+							+ object.getString("params") + "]");
+					Assert.assertTrue(expNum <= actNum);
+				}
 			}
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 			e.printStackTrace();
